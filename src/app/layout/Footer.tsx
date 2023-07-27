9a73f08bcd43d4fc8a008d0410f5c401322b5331
john7rho <johnkrho@gmail.com>
2023-07-27 14:27:44 -0700

    credit

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -69,7 +69,8 @@ export default function Footer() {
             >
               <IoIosGitBranch fontSize="0.9rem" />
               <Typography sx={{ ml: 0.5, mt: 0.1, fontSize: "0.6rem" }}>
-                main
+                Link to site template repo (thanks to
+                https://github.com/noworneverev)
               </Typography>
             </Box>
             {/* </Link> */}
