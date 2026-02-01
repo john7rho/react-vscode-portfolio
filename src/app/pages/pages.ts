6f8909418f7e6bfdabf79a5b3ff8643c61b52d94
John Rho <john7rho@users.noreply.github.com>
2026-01-31 19:14:48 -0500

    Hide education and coursework tabs

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -1,9 +1,9 @@
 export const pages = [
   { index: 0, name: 'overview.md', route: '/overview' },
-  { index: 1, name: 'education.md', route: '/education' },
-  { index: 2, name: 'projects.md', route: '/projects' },
-  { index: 3, name: 'coursework.md', route: '/coursework' },
-  { index: 4, name: 'books.md', route: '/books' },
-  { index: 5, name: 'investing.md', route: '/investing' },
-  { index: 6, name: 'settings.tsx', route: '/settings' },
+  // { index: 1, name: 'education.md', route: '/education' },
+  { index: 1, name: 'projects.md', route: '/projects' },
+  // { index: 3, name: 'coursework.md', route: '/coursework' },
+  { index: 2, name: 'books.md', route: '/books' },
+  { index: 3, name: 'investing.md', route: '/investing' },
+  { index: 4, name: 'settings.tsx', route: '/settings' },
 ];
